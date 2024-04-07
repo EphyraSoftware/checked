cd22c783247782c9b8b925c75661afcbfa5b95cb
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-04-07 13:09:29 +0100

    Add mismatched signature test

--- checked_cli/tests/happ_integration.rs
+++ checked_cli/tests/happ_integration.rs
@@ -158,7 +158,7 @@ async fn fetch_asset_signed_by_others() -> anyhow::Result<()> {
 
     let admin_port = add_admin_port(conductor.sweet_handle()).await?;
 
-    for i in 0..10 {
+    for i in 0..6 {
         publish_asset_signature(
             conductor.sweet_handle(),
             &url,
@@ -220,6 +220,93 @@ async fn fetch_asset_signed_by_others() -> anyhow::Result<()> {
         .unwrap();
     assert_eq!(5, recent_signatures.passed_signatures.len());
     assert!(recent_signatures.failed_signatures.is_empty());
+
+    assert!(fetch_info
+        .reports
+        .iter()
+        .find(|r| r.reason == FetchCheckSignatureReason::RandomHistorical)
+        .is_none());
+
+    assert!(fetch_info
+        .reports
+        .iter()
+        .find(|r| matches!(r.reason, FetchCheckSignatureReason::Pinned(_)))
+        .is_none());
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn fetch_asset_signed_by_others_with_mismatches() -> anyhow::Result<()> {
+    let conductor = SweetConductor::from_standard_config().await;
+
+    let (addr, _fs_abort_handle) = start_sample_file_server().await;
+    let url = format!("http://{}:{}/test.txt", addr.ip(), addr.port());
+
+    let admin_port = add_admin_port(conductor.sweet_handle()).await?;
+
+    for i in 0..5 {
+        publish_asset_signature(
+            conductor.sweet_handle(),
+            &url,
+            admin_port,
+            format!("checked-{i}"),
+            i == 2 || i == 4,
+        )
+            .await
+            .context("Couldn't publish asset signature")?;
+    }
+
+    install_checked_app(conductor.sweet_handle(), "checked").await?;
+
+    let dir = tempfile::tempdir()?;
+
+    let name = "test_generate".to_string();
+    generate(GenerateArgs {
+        name: name.clone(),
+        port: Some(admin_port),
+        password: Some("test".to_string()),
+        distribute: Some(true),
+        path: Some(dir.as_ref().to_path_buf()),
+        app_id: None,
+    })
+        .await?;
+
+    let fetch_info = fetch(FetchArgs {
+        url: url.clone(),
+        port: admin_port,
+        name,
+        output: Some(dir.as_ref().to_path_buf()),
+        password: Some("test".to_string()),
+        path: Some(dir.as_ref().to_path_buf()),
+        allow_no_signatures: Some(false),
+        sign: Some(true),
+        app_id: None,
+    })
+        .await?;
+
+    assert!(fetch_info.signature_path.is_some());
+
+    let zome = get_zome_handle(&conductor, "checked", "fetch").await;
+
+    let signatures: Vec<AssetSignatureResponse> = conductor
+        .call_fallible(&zome, "get_my_asset_signatures", ())
+        .await?;
+
+    assert_eq!(1, signatures.len());
+    assert_eq!(url, signatures[0].fetch_url);
+    assert_eq!(
+        std::fs::read_to_string(fetch_info.signature_path.unwrap())?,
+        signatures[0].signature
+    );
+
+    let recent_signatures = fetch_info
+        .reports
+        .iter()
+        .find(|r| r.reason == FetchCheckSignatureReason::RandomRecent)
+        .unwrap();
+    assert_eq!(3, recent_signatures.passed_signatures.len());
+    assert_eq!(2, recent_signatures.failed_signatures.len());
 
     assert!(fetch_info
         .reports
