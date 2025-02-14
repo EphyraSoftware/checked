159bd1dc472cb69a309e92baf212a62e89da0c12
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-02-14 17:01:32 +0000

    chore: Retry tests on CI

--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     fileParallelism: false, // run tests serially, multi-conductor tests max out the machine anyway
-    testTimeout: 60*1000*3 // 3  mins
+    testTimeout: 60*1000*5, // 5 mins
+    retry: 3, // Add a retry, some of the tests are flaky but it's most likely because of Holochain and not the tests
   },
 })
 
