c2ea8ce19ea62c012f4de3667a4afecab3965247
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-11 00:27:30 +0000

    chore: Fix signal handling

--- ui/src/signals.ts
+++ ui/src/signals.ts
@@ -1,4 +1,4 @@
-import { AppClient } from "@holochain/client";
+import {AppClient, Signal, SignalType} from "@holochain/client";
 import { KeyCollectionWithKeys } from "./store/key-collections-store";
 import {
   CheckedSigningKeysSignal,
@@ -25,59 +25,63 @@ export const registerSignalHandler = (
     };
   }>,
 ) => {
-  client.on("signal", (signal) => {
-    if (signal.zome_name === "signing_keys") {
-      const payload = signal.payload as CheckedSigningKeysSignal;
+  client.on("signal", (sig: Signal) => {
+    if (SignalType.App in sig) {
+      const signal = sig[SignalType.App];
 
-      if (payload.type === "EntryCreated") {
-        const app_entry = payload.app_entry;
-        const action = payload.action;
+      if (signal.zome_name === "signing_keys") {
+        const payload = signal.payload as CheckedSigningKeysSignal;
 
-        if (app_entry.type === "VerificationKeyDist") {
-          if (myKeysStore) {
-            myKeysStore.pushVfKeyDist({
-              verification_key_dist: {
-                verification_key: app_entry.verification_key,
-                key_type: app_entry.key_type,
+        if (payload.type === "EntryCreated") {
+          const app_entry = payload.app_entry;
+          const action = payload.action;
+
+          if (app_entry.type === "VerificationKeyDist") {
+            if (myKeysStore) {
+              myKeysStore.pushVfKeyDist({
+                verification_key_dist: {
+                  verification_key: app_entry.verification_key,
+                  key_type: app_entry.key_type,
+                  name: app_entry.name,
+                  expires_at: app_entry.expires_at,
+                  marks: [], // Expect no marks initially
+                },
+                key_dist_address: action.hashed.hash,
+                created_at: action.hashed.content.timestamp,
+                reference_count: 0, // Assume newly created keys have a 0 reference count
+                author: action.hashed.content.author,
+              });
+            }
+          } else if (app_entry.type === "KeyCollection") {
+            if (keyCollectionsStore) {
+              keyCollectionsStore.pushKeyCollection({
                 name: app_entry.name,
-                expires_at: app_entry.expires_at,
-                marks: [], // Expect no marks initially
-              },
-              key_dist_address: action.hashed.hash,
-              created_at: action.hashed.content.timestamp,
-              reference_count: 0, // Assume newly created keys have a 0 reference count
-              author: action.hashed.content.author,
-            });
+                verification_keys: [],
+              });
+            }
+          } else {
+            console.warn("Unknown app entry type in signal", signal);
           }
-        } else if (app_entry.type === "KeyCollection") {
-          if (keyCollectionsStore) {
-            keyCollectionsStore.pushKeyCollection({
-              name: app_entry.name,
-              verification_keys: [],
-            });
-          }
-        } else {
-          console.warn("Unknown app entry type in signal", signal);
         }
-      }
-    } else if (signal.zome_name === "fetch") {
-      const payload = signal.payload as CheckedFetchSignal;
+      } else if (signal.zome_name === "fetch") {
+        const payload = signal.payload as CheckedFetchSignal;
 
-      if (payload.type === "EntryCreated") {
-        const app_entry = payload.app_entry;
-        const action = payload.action;
+        if (payload.type === "EntryCreated") {
+          const app_entry = payload.app_entry;
+          const action = payload.action;
 
-        if (app_entry.type === "AssetSignature") {
-          if (myAssetSignaturesStore) {
-            myAssetSignaturesStore.pushAssetSignature({
-              fetch_url: app_entry.fetch_url,
-              signature: app_entry.signature,
-              key_dist_address: app_entry.key_dist_address,
-              created_at: action.hashed.content.timestamp,
-            });
+          if (app_entry.type === "AssetSignature") {
+            if (myAssetSignaturesStore) {
+              myAssetSignaturesStore.pushAssetSignature({
+                fetch_url: app_entry.fetch_url,
+                signature: app_entry.signature,
+                key_dist_address: app_entry.key_dist_address,
+                created_at: action.hashed.content.timestamp,
+              });
+            }
+          } else {
+            console.warn("Unknown app entry type in signal", signal);
           }
-        } else {
-          console.warn("Unknown app entry type in signal", signal);
         }
       }
     }
